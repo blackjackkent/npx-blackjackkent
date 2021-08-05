4bbdc00ee31e4c186dbe3321269c0e02718d9918
Rosalind W <blackjacksoftwaredev@gmail.com>
2021-08-04 22:46:12 -0500

    Initial commit

--- card.js
+++ card.js
@@ -19,112 +19,111 @@ clear();
 const res = fs.readFileSync(path.resolve(__dirname, "data.json"));
 const user_data = JSON.parse(res);
 const {
-    user_name,
-    user_email,
-    twitter_username,
-    linkedin_username,
-    github_username,
-    personal_site,
-    npx_card_handle,
-    job_title,
-    resume_url,
+	user_name,
+	user_email,
+	twitter_username,
+	github_username,
+	linkedin_username,
+	personal_site,
+	discord,
+	patreon,
+	tiktok,
+	npx_card_handle,
+	job_title,
 } = user_data;
 
 const prompt = inquirer.createPromptModule();
 
 const questions = [
-    {
-        type: "list",
-        name: "action",
-        message: "What you want to do?",
-        choices: [
-            //// Send an email
-            {
-                name: `Send me an ${chalk.green.bold("email")}?`,
-                value: () => {
-                    open(`mailto:${user_email}`);
-                    console.log("\nDone, see you soon at inbox.\n");
-                },
-            },
-            //// Download Resume
-            {
-                name: `Download my ${chalk.magentaBright.bold("Resume")}?`,
-                value: () => {
-                    // cliSpinners.dots;
-                    const loader = ora({
-                        text: " Downloading Resume",
-                        spinner: cliSpinners.material,
-                    }).start();
-                    let pipe = request(`${resume_url}`).pipe(
-                        fs.createWriteStream(`./${npx_card_handle}-resume.html`)
-                    );
-                    pipe.on("finish", function () {
-                        let downloadPath = path.join(
-                            process.cwd(),
-                            `${npx_card_handle}-resume.html`
-                        );
-                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
-                        open(downloadPath);
-                        loader.stop();
-                    });
-                },
-            },
-            //// Quit
-            {
-                name: "Just quit.",
-                value: () => {
-                    console.log("Hasta la vista.\n");
-                },
-            },
-        ],
-    },
+	{
+		type: "list",
+		name: "action",
+		message: "What you want to do?",
+		choices: [
+			//// Send an email
+			{
+				name: `Send me an ${chalk.green.bold("email")}.`,
+				value: () => {
+					open(`mailto:${user_email}`);
+					console.log("\nOpened in your mail client!\n");
+				},
+			},
+			//// Join Discord
+			{
+				name: `Join my ${chalk.magentaBright.bold("Discord server")}.`,
+
+				value: () => {
+					open(`https://discord.com/invite/${discord}`);
+					console.log("\nOpened in browser!\n");
+				},
+			},
+			//// Quit
+			{
+				name: "Quit.",
+				value: () => {
+					console.log("Thanks for visiting!\n");
+				},
+			},
+		],
+	},
 ];
 
 const data = {
-    name: chalk.bold.green(`                  ${user_name}`),
-    // work: `${chalk.white("Software Engineer at")} ${chalk.hex("#2b82b2").bold("ClearTax")}`,
-    work: `${chalk.white(`${job_title}`)}`,
-    twitter: chalk.gray("https://twitter.com/") + chalk.cyan(`${twitter_username}`),
-    github: chalk.gray("https://github.com/") + chalk.green(`${github_username}`),
-    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue(`${linkedin_username}`),
-    web: chalk.cyan(`${personal_site}`),
-    npx: chalk.red("npx") + " " + chalk.white(`${npx_card_handle}`),
+	name: chalk.bold.blue(`               ${user_name}`),
+	// work: `${chalk.white("Software Engineer at")} ${chalk.hex("#2b82b2").bold("ClearTax")}`,
+	work: `    ${chalk.white(`${job_title}`)}`,
+	twitter:
+		chalk.gray("https://twitter.com/") + chalk.cyan(`${twitter_username}`),
+	github: chalk.gray("https://github.com/") + chalk.green(`${github_username}`),
+	web: `             ${chalk.cyan(`${personal_site}`)}`,
+	discord:
+		chalk.gray("https://discord.com/invite/") + chalk.magenta(`${discord}`),
+	patreon: chalk.gray("https://www.patreon.com/") + chalk.red(`${patreon}`),
+	npx: chalk.red("npx") + " " + chalk.white(`${npx_card_handle}`),
 
-    labelWork: chalk.white.bold("       Work:"),
-    labelTwitter: chalk.white.bold("    Twitter:"),
-    labelGitHub: chalk.white.bold("     GitHub:"),
-    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
-    labelWeb: chalk.white.bold("        Web:"),
-    labelCard: chalk.white.bold("       Card:"),
+	labelWork: chalk.white.bold("       Work:"),
+	labelTwitter: chalk.white.bold("    Twitter:"),
+	labelGitHub: chalk.white.bold("     GitHub:"),
+	labelDiscord: chalk.white.bold("    Discord:"),
+	labelPatreon: chalk.white.bold("    Patreon:"),
+	labelWeb: chalk.white.bold("        Web:"),
+	labelCard: chalk.white.bold("       Card:"),
 };
 
 const me = boxen(
-    [
-        `${data.name}`,
-        ``,
-        `${data.labelWork}  ${data.work}`,
-        ``,
-        `${data.labelTwitter}  ${data.twitter}`,
-        `${data.labelGitHub}  ${data.github}`,
-        `${data.labelLinkedIn}  ${data.linkedin}`,
-        `${data.labelWeb}  ${data.web}`,
-        ``,
-        `${data.labelCard}  ${data.npx}`,
-        ``,
-        `${chalk.italic("I am currently looking for new opportunities,")}`,
-        `${chalk.italic("my inbox is always open. Whether you have a")}`,
-        `${chalk.italic("question or just want to say hi, I will try ")}`,
-        `${chalk.italic("my best to get back to you!")}`,
-    ].join("\n"),
-    {
-        margin: 1,
-        float: "center",
-        padding: 1,
-        borderStyle: "single",
-        borderColor: "green",
-    }
+	[
+		`${data.name}`,
+		``,
+		`${data.work}`,
+		`${data.web}`,
+		``,
+		`${data.labelTwitter}  ${data.twitter}`,
+		`${data.labelGitHub}  ${data.github}`,
+		`${data.labelDiscord}  ${data.discord}`,
+		`${data.labelPatreon}  ${data.patreon}`,
+		``,
+		`${data.labelCard}  ${data.npx}`,
+		``,
+		`${chalk.italic(
+			"Hi, I'm Roz! I'm a software developer and content creator"
+		)}`,
+		`${chalk.italic("from Chicago, IL. I want to make software that makes")}`,
+		`${chalk.italic("life better, and help you do the same!")}`,
+		``,
+		`${chalk.bold.white("Gaming and software dev weekly on Twitch: ")}`,
+		`        ${chalk.bold.cyan("http://www.twitch.tv/blackjack_kent")}`,
+		`${chalk.bold.white("Coding tutorials on TikTok: ")}`,
+		`        ${chalk.bold.cyan("http://www.tiktok.com/@blackjackkent")}`,
+	].join("\n"),
+	{
+		margin: 1,
+		float: "center",
+		padding: 1,
+		borderStyle: "single",
+		borderColor: "blue",
+	}
 );
 
 console.log(me);
 
-prompt(questions).then(answer => answer.action());
+prompt(questions).then((answer) => answer.action());
